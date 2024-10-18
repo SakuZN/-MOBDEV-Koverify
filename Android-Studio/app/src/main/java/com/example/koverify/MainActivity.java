0c5029ed43248babe60cc43d074a4b88d578f136
Rafa <58285574+JsRphlMrtnz@users.noreply.github.com>
2024-10-18 18:09:45 +0800

    feat: add splashscreen design

--- Android-Studio/app/src/main/java/com/example/koverify/MainActivity.java
+++ Android-Studio/app/src/main/java/com/example/koverify/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.koverify;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,17 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView prompt;
+    Button nextButton;
+    int index = 0;
+
+    String[] prompts = {
+            "Welcome to Koverify!",
+            "Find your products’ nutritional data fast",
+            "Scan a barcode or search for your item",
+            "⚠️ Not made to replace professional medical help"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        prompt = findViewById(R.id.prompt);
+        nextButton = findViewById(R.id.next);
+    }
+
+    public void nextPrompt(View view) {
+        if (index != prompts.length - 1) {
+            index++;
+            prompt.setText(prompts[index]);
+            if (index != 0) {
+                nextButton.setText("Continue");
+            }
+        }
+        else {
+            // add new activity here
+            finish();
+        }
     }
 }
